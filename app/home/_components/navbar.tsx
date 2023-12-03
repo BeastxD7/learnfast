2a536ce6a340307686a83f49015042c79a2868cd
Shashank Devadiga <beast.steam002@gmail.com>
2023-12-03 21:54:27 +0530

    Structured Routes

--- app/home/_components/navbar.tsx
+++ app/home/_components/navbar.tsx
@@ -3,6 +3,8 @@ import Link from "next/link";
 import { Logo } from "@/components/logo";
 import { Button } from "@/components/ui/button";
 import { ModeToggle } from "@/components/mode-toggle";
+import { UserButton } from "@clerk/nextjs";
+
 
 export const Navbar = () => {
   return (
@@ -10,19 +12,10 @@ export const Navbar = () => {
       <div className="md:max-w-screen-2xl mx-auto flex items-center w-full justify-between">
         <Logo />
         <div className="space-x-4 md:block md:w-auto flex items-center justify-between w-full">
-          <Button size="sm" variant="outline" asChild>
-            <Link href="/sign-in">
-              Login
-            </Link>
-          </Button>
-          <Button size="sm" asChild>
-            <Link href="/sign-up">
-              Try it out!
-            </Link>
-          </Button>
-          <ModeToggle />
+        <ModeToggle />
         </div>
       </div>
+        <UserButton afterSignOutUrl="/" /> 
     </div>
   );
 };
