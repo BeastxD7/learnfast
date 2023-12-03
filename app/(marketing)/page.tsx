ae7afc1f69afa3f7c14f587e64ca116cf0ab3d0e
Shashank Devadiga <beast.steam002@gmail.com>
2023-12-03 21:31:34 +0530

    Authentication

---
+++ app/(marketing)/page.tsx
@@ -0,0 +1,34 @@
+import Link from "next/link";
+import localFont from "next/font/local";
+import { Poppins } from "next/font/google";
+import { BadgeCheck } from "lucide-react";
+
+
+import { cn } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
+
+const headingFont = localFont({
+  src: "../../public/fonts/font.woff2"
+});
+
+const textFont = Poppins({
+  subsets: ["latin"],
+  weight: [
+    "100",
+    "200",
+    "300",
+    "400",
+    "500",
+    "600",
+    "700",
+    "800",
+    "900"
+  ],
+});
+
+const MarketingPage = () => {
+  return (
+    <div> <h1>Courses</h1></div>
+  );
+};
+export default MarketingPage;
